10b7244378666b351d1423554e6c7bebccf55d16
Farhan Fathurrahman <frhnfath@users.noreply.github.com>
2021-11-04 17:32:37 +0700

    insert data and modal box

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,9 +2,10 @@
 
     <div class="row">
         <div class="col-6">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Launch demo modal
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+                Tambah Data Mahasiswa
             </button>
+            <br><br>
             <h3>Daftar Mahasiswa</h3>
                 <ul class="list-group">
                     <?php foreach( $data['mhs'] as $mhs) : ?>
@@ -21,21 +22,45 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama" name="nama" >
+            </div>
+            <div class="form-group">
+                <label for="nrp">NRP</label>
+                <input type="text" class="form-control" id="nrp" name="nrp" >
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" >
+            </div>
+            <div class="form-group">
+                <label for="jurusan">Jurusan</label>
+                <select class="form-control" id="jurusan" name="jurusan">
+                <option value="Ilmu Komputer">Ilmu Komputer</option>
+                <option value="Biologi">Biologi</option>
+                <option value="Fisika">Fisika</option>
+                <option value="Teknologi Pangan">Teknologi Pangan</option>
+                <option value="Teknik Planologi">Teknik Planologi</option>
+                <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                </select>
+            </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
       </div>
     </div>
   </div>
